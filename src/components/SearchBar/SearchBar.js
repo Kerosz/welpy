dcfcb0a22a68ce5c6c05f2068a461f77ce18481d
Kerosz <ch.andrei18@gmail.com>
2020-07-23 20:36:11 +0300

    add search bar functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,6 +14,10 @@ class SearchBar extends React.Component {
 			'Highest Rated': 'rating',
 			'Most Reviewed': 'review_count',
 		};
+
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
 	}
 
 	getSortByClass(sortByOption) {
@@ -24,16 +28,40 @@ class SearchBar extends React.Component {
 		this.setState({ sortBy: sortByOption });
 	}
 
+	handleTermChange(e) {
+		const newTerm = e.target.value;
+		this.setState({ term: newTerm });
+	}
+
+	handleLocationChange(e) {
+		const newLocation = e.target.value;
+		this.setState({ location: newLocation });
+	}
+
+	handleSearch(e) {
+		const { term, location, sortBy } = this.state;
+		this.searchApi(term, location, sortBy);
+
+		e.preventDefault();
+	}
+
 	renderSortByOptions() {
 		return Object.keys(this.sortByOptions).map((option) => {
 			let sortByOptionValue = option;
 
 			return (
-				<li key={sortByOptionValue} className={this.getSortByClass()}>
+				<li
+					key={sortByOptionValue}
+					className={this.getSortByClass(sortByOptionValue)}
+					onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
 					{option}
 				</li>
 			);
 		});
+	}
+
+	searchApi(term, location, sortBy) {
+		console.log(`Searching for ${term} in ${location} by ${sortBy}`);
 	}
 
 	render() {
@@ -43,11 +71,16 @@ class SearchBar extends React.Component {
 					<ul>{this.renderSortByOptions()}</ul>
 				</div>
 				<div className="SearchBar-fields">
-					<input placeholder="Search Businesses" />
-					<input placeholder="Where?" />
+					<input
+						placeholder="Search Businesses"
+						onChange={this.handleTermChange}
+					/>
+					<input placeholder="Where?" onChange={this.handleLocationChange} />
 				</div>
 				<div className="SearchBar-submit">
-					<submit className="btn">Let's Go</submit>
+					<submit className="btn" onClick={this.handleSearch}>
+						Let's Go
+					</submit>
 				</div>
 			</div>
 		);
