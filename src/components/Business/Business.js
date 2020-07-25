7e2b4b28ba93403147da91c7cf72c08ddcb8bd74
Kerosz <ch.andrei18@gmail.com>
2020-07-26 00:33:33 +0300

    update list results styles

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import './Business.css';
+import placeholder from '../../img/placeholder.png';
 
 class Business extends React.Component {
 	render() {
@@ -12,27 +14,31 @@ class Business extends React.Component {
 			zipCode,
 			category,
 			rating,
-			reviewCount,
+			// reviewCount,
 		} = this.props.business;
+
+		const decideWhichImage = imageSrc ? imageSrc : placeholder;
 
 		return (
 			<div className='Business'>
-				<div className='image-container'>
-					<img src={imageSrc} alt='' />
+				<div className='image__container'>
+					<img src={decideWhichImage} alt='Placeholder' />
 				</div>
-				<h2 className='Business__name'>{name}</h2>
-				<div className='Business-information'>
-					<div className='Business-address'>
-						<p>{address}</p>
-						<p>{city}</p>
-						<p>
-							{state} {zipCode}
-						</p>
-					</div>
-					<div className='Business-reviews'>
-						<h3>{category}</h3>
-						<h3 className='rating'>{rating} stars</h3>
-						<p>{reviewCount} reviews</p>
+				<div className='text__container'>
+					<h2 className='Business__name'>{name}</h2>
+					<h3 className='rating'>{rating} stars</h3>
+					<div className='Business__information'>
+						<div className='Business__address'>
+							<p className='Address__label'>{address}</p>
+							<p className='Address__label'>{city}</p>
+							<p className='Address__label'>
+								{state} {zipCode}
+							</p>
+						</div>
+						<div className='Business__reviews'>
+							<h4 className='category'>{category}</h4>
+							{/* <p>{reviewCount} reviews</p> */}
+						</div>
 					</div>
 				</div>
 			</div>
